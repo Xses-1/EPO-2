5d975765d8a896fa17f72e602ef362932e129e34
Thijs J.A. van Esch <me@thijsvanesch.com>
2023-04-30 15:56:07 +0200

    Added preprocessor logic for determining serial communication format and port, according to target platform

---
+++ Controller/Serial/params.h
@@ -0,0 +1,24 @@
+//
+// Created by Thijs J.A. van Esch on 30-4-23.
+//
+
+#ifndef EPO2_PARAMS_H
+#define EPO2_PARAMS_H
+
+#include "platform.h"
+
+#if WINDOWS
+# 	define BAUD CBR_9600
+# 	define BYTESIZE 8
+#	define PARITY NOPARITY
+# 	define STOPBITS ONESTOPBIT
+#	define PORT "COM5"
+#else
+#	define BAUD B9600
+# 	define BYTESIZE 8
+#	define PARITY 0
+# 	define STOPBITS 0
+#	define PORT "/dev/ttyUSB0"
+#endif
+
+#endif //EPO2_PARAMS_H
