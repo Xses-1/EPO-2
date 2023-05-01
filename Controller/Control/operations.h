4db22827871b6086861e7e6b566d2d24c3f17e65
Thijs J.A. van Esch <me@thijsvanesch.com>
2023-05-01 15:31:45 +0200

    Defined macros for possible robot operations

---
+++ Controller/Control/operations.h
@@ -0,0 +1,12 @@
+//
+// Created by Thijs J.A. van Esch on 1-5-2023.
+//
+
+#ifndef EPO2_OPERATIONS_H
+#define EPO2_OPERATIONS_H
+
+#define ROBOT_FORWARD 		0b01
+#define ROBOT_ROTATELEFT 	0b10
+#define ROBOT_ROTATERIGHT 	0b10
+
+#endif//EPO2_OPERATIONS_H
