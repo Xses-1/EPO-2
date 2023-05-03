2b68756181c3a5113bfdbcf801573745bf16e2ed
Thijs J.A. van Esch <me@thijsvanesch.com>
2023-05-03 13:35:13 +0200

    Added functions for initializing a new board

---
+++ Controller/Control/Algorithm/defaults.h
@@ -0,0 +1,18 @@
+//
+// Created by Thijs J.A. van Esch on 3-5-23.
+//
+
+#ifndef EPO2_DEFAULTS_H
+#define EPO2_DEFAULTS_H
+
+#define N_ROWS 13
+#define N_COLS 13
+
+#include "types.h"
+
+/**
+ * Returns a newly created board with all default values.
+ */
+Board *getDefaultBoard();
+
+#endif//EPO2_DEFAULTS_H
---
+++ Controller/Control/Algorithm/types.h
@@ -0,0 +1,12 @@
+//
+// Created by Thijs J.A. van Esch on 3-5-23.
+//
+
+#ifndef EPO2_TYPES_H
+#define EPO2_TYPES_H
+
+typedef int **Board;
+
+
+
+#endif//EPO2_TYPES_H
