99fee394395fcf5cb7f1eafe98ff431559349b19
Thijs J.A. van Esch <me@thijsvanesch.com>
2023-05-03 12:35:46 +0200

    Improved description of the `parseArguments` function

--- Controller/Control/arguments.h
+++ Controller/Control/arguments.h
@@ -26,9 +26,18 @@ typedef struct PROGRAM_ARGS PROGRAM_ARGS;
 /**
  * Parses the given command line arguments.
  *
+ * The proper way to call this application is by calling:
+ *
+ * 	[executable] [start] [station, ...]
+ *
+ * Where both `start` and all entries for `station` are integers that represent
+ * a station on the maze. This means that all of these numbers are to be
+ * positive numbers in the range [1, 12].
+ *
  * If a nonzero value is returned by this function, it means invalid arguments
  * are given and the program needs to exit after prompting the user to input
- * valid data.
+ * valid data. If this is the case, an error message will be printed by this
+ * function.
  */
 int parseArguments(int argc, const char *argv[], PROGRAM_ARGS *args);
 
