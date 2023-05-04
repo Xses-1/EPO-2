ffecb4610b7ac6c651561a0a5679f60ecccd24aa
Thijs J.A. van Esch <me@thijsvanesch.com>
2023-05-04 10:42:37 +0200

    Updated robot operations

--- Controller/Control/operations.h
+++ Controller/Control/operations.h
@@ -5,9 +5,11 @@
 #ifndef EPO2_OPERATIONS_H
 #define EPO2_OPERATIONS_H
 
-#define ROBOT_NOOP 		0b01
-#define ROBOT_FORWARD 		0b01
-#define ROBOT_ROTATELEFT 	0b10
-#define ROBOT_ROTATERIGHT 	0b10
+#define ROBOT_NOOP 		0b000
+#define ROBOT_LEFT	 	0b001
+#define ROBOT_RIGHT 		0b010
+#define ROBOT_FORWARD 		0b011
+#define ROBOT_UTURN 		0b100
+#define ROBOT_STOP 		0b101
 
 #endif//EPO2_OPERATIONS_H
