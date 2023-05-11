3db6c7e80ef625708554e5330255579fc9b2d956
Wiktor Tomanek <wiktort52@gmail.com>
2023-05-11 09:25:51 +0200

    V3

--- Minedetector.vhd
+++ Minedetector.vhd
@@ -1,4 +1,4 @@
-library IEEE;
+ibrary IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
@@ -7,7 +7,11 @@ entity mine_sensor is
 	port (	clk		: in  std_logic;
 		reset		: in  std_logic;
 		sensor		: in  std_logic;
-		mine_out	: out std_logic
+		mine_out	: out std_logic;
+		led_1		: out std_logic;
+		led_2		: out std_logic;	
+		led_3		: out std_logic;
+		led_4		: out std_logic
 	);
 end entity mine_sensor;
 
@@ -37,6 +41,11 @@ begin
 		begin	
 		case state is
 			when reset_state =>
+				led_1 <= '1';
+				led_2 <= '0';
+				led_3 <= '0';
+				led_4 <= '0';
+
 				mine_out <= '0';
 				new_count <= (others => '0');
 
@@ -47,6 +56,10 @@ begin
 				end if;
 
 			when count_state =>
+				led_1 <= '0';
+				led_2 <= '1';
+				led_3 <= '0';
+				led_4 <= '0';
 				mine_out <= '0';
 				if (sensor = '0') then
 					new_count <= count + 1;
@@ -57,6 +70,10 @@ begin
 				end if;
 
 			when compare_state =>
+				led_1 <= '0';
+				led_2 <= '0';
+				led_3 <= '1';
+				led_4 <= '0';
 				mine_out <= '0';
 				new_count <= count;
 				if (count > treshhold) then
@@ -66,6 +83,10 @@ begin
 				end if;
 
 			when detected_state =>
+				led_1 <= '0';
+				led_2 <= '0';
+				led_3 <= '0';
+				led_4 <= '1';
 				mine_out <= '1';
 				new_count <= count;
 				next_state <= detected_state;	
