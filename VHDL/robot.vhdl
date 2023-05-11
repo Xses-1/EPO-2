3f0b8233371ed724fa1c7d2fc44b9406102054a0
Chantal <chantalrhchen@hotmail.com>
2023-05-11 10:24:34 +0200

    added: comp uart and controller, see line 4-8

--- VHDL/robot.vhdl
+++ VHDL/robot.vhdl
@@ -1,6 +1,11 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+-- ROBOT ENTITY MOET NOG GEWIJZIGD WORDEN.
+-- MINE SENSOR COMPONENT MOET NOG WORDEN TOEGEVOEGD EN GEPORTMAPPED.
+-- MINE SENSOR MOET NOG IN DE CONTROLLER WORDEN GEPORTMAPPED.
+-- UART GROEPJE KIJKEN NAAR COMPONENT EN PORT MAP SVP.
+
 entity robot is
 	port (  clk             : in    std_logic;
 		reset           : in    std_logic;
@@ -37,7 +42,19 @@ component controller is
 		sensor_m		: in	std_logic;
 		sensor_r		: in	std_logic;
 
-		count_in		: in	std_logic_vector (19 downto 0);  -- Please enter upper bound
+		count_in		: in	std_logic_vector (19 downto 0);  
+
+--new inputs
+		data_in			: in 	std_logic_vector (7 downto 0);		
+		data_ready		: in	std_logic;
+		mine_s			: in	std_logic;
+
+
+--new outputs
+		data_out		: out	std_logic_vector (7 downto 0);	
+		write_data		: out	std_logic;
+		read_data		: out	std_logic;
+--		
 		count_reset		: out	std_logic;
 
 		motor_l_reset		: out	std_logic;
@@ -68,10 +85,32 @@ component motorcontrol is
 	);
 end component motorcontrol;
 
+component uart is
+	port (
+        clk             : in  std_logic;
+        reset           : in  std_logic;
+
+        rx              : in  std_logic;
+        tx              : out std_logic;
+
+        data_in         : in  std_logic_vector (7 downto 0);
+        buffer_empty    : out std_logic;
+        write           : in  std_logic;
+
+        data_out        : out std_logic_vector (7 downto 0);
+        data_ready      : out std_logic;
+        read            : in  std_logic
+    );
+end component uart;
 
 
 signal sensor_l, sensor_m, sensor_r, count_rs, motor_l_rs, motor_l_d, motor_r_rs, motor_r_d: std_logic;
 signal count: std_logic_vector(19 downto 0); 
+
+-- new signals
+signal data_uico: std_logic_vector (7 downto 0);  --uart in controller out
+signal data_uoci: std_logic_vector (7 downto 0); -- uart out controller in
+signal write_sig, read_sig, data_ready_sig: std_logic; 
 
 begin
 LB1: inputbuffer port map(	clk		=>	clk,
@@ -84,16 +123,44 @@ LB1: inputbuffer port map(	clk		=>	clk,
 				sensor_m_out	=>	sensor_m,
 				sensor_r_out	=>	sensor_r);
 
+-- UART groepje editen en checken graag.
+U1:	uart port map(
+					clk  => clk,
+					reset => reset,
 
+					rx => ,	
+					tx => ,
 
-LB2: controller port map(	clk			=>	clk,
+					data_in => data_uico,     
+					buffer_empty =>   
+					write => write_sig,     
+
+					data_out => data_uoci,
+					data_ready => data_ready_sig,  
+					read => read_sig    
+);
+
+LB2: controller port map(	
+				clk			=>	clk,
 				reset			=>	reset,
 
 				sensor_l		=>	sensor_l,
 				sensor_m		=>	sensor_m,
 				sensor_r		=>	sensor_r,
 
-				count_in		=>	count,
+				count_in		=>  count,
+
+--new inputs
+				data_in			=> data_uoci  ,
+				data_ready		=> data_ready_sig, 
+				mine_s			=> ,  --port map naar sensor signal
+
+
+--new outputs
+				data_out		=> data_uico,
+				write_data		=> write_sig,
+				read_data		=> read_sig,
+--
 
 				count_reset		=>	count_rs,
 
@@ -107,9 +174,6 @@ LB2: controller port map(	clk			=>	clk,
 LB3: timebase port map(		clk		=>	clk,
 				reset		=>	count_rs,
 				count_out	=>	count);
-
-
-
 
 MCL: motorcontrol port map(	clk		=>	clk,
 				reset		=>	motor_l_rs,
