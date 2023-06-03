08c6257ae12cf05a2312bc94e6f3f6ccfb1780cf
Wilson <98850345+wilsonrng@users.noreply.github.com>
2023-06-03 16:03:38 +0200

    Mapped the new periodical reset signal between the motorcontrols and timebase

--- VHDL/robot.vhdl
+++ VHDL/robot.vhdl
@@ -7,9 +7,11 @@ use IEEE.std_logic_1164.all;
 -- UART GROEPJE KIJKEN NAAR COMPONENT EN PORT MAP SVP.
 
 entity robot is
-	port (  clk             : in    std_logic;
+	port ( 
+	
+		clk             : in    std_logic;
 		reset           : in    std_logic;
-
+		
 		sensor_l_in     : in    std_logic;
 		sensor_m_in     : in    std_logic;
 		sensor_r_in     : in    std_logic;
@@ -21,7 +23,17 @@ entity robot is
 		buffer_empty	: out	std_logic;
 
 		motor_l_pwm     : out   std_logic;
-		motor_r_pwm     : out   std_logic
+		motor_r_pwm     : out   std_logic;
+		
+		led_7 : out std_logic;
+		led_6 : out std_logic;
+		led_5 : out std_logic;
+		led_4 : out std_logic;
+		led_3 : out std_logic;
+		led_2 : out std_logic;
+		led_1 : out std_logic;
+		led_0 : out std_logic
+	
 	);
 end entity robot;
 
@@ -68,6 +80,7 @@ component controller is
 
 		motor_r_reset		: out	std_logic;
 		motor_r_direction	: out	std_logic
+
 	);
 
 end component controller;
@@ -76,6 +89,9 @@ end component controller;
 component timebase is
 	port (  clk             : in std_logic;
         	reset           : in std_logic;
+	
+		reset_out	: out std_logic;					-- New port for periodic reset.	
+
         	count_out       : out std_logic_vector (19 downto 0)
  	) ;
  end component timebase ;
@@ -87,6 +103,8 @@ component motorcontrol is
 		direction	: in	std_logic;
 		count_in	: in	std_logic_vector (19 downto 0);  -- Please enter upper bound
 
+		reset_in	: in std_logic;					-- New port for periodic reset.	
+	
 		pwm		: out	std_logic
 	);
 end component motorcontrol;
@@ -116,8 +134,9 @@ component mine_sensor_top is
 	);
 end component mine_sensor_top;
 
-signal sensor_l, sensor_m, sensor_r, count_rs, motor_l_rs, motor_l_d, motor_r_rs, motor_r_d: std_logic;
+signal sensor_l, sensor_m, sensor_r, count_rs, reset_periodical, motor_l_rs, motor_l_d, motor_r_rs, motor_r_d: std_logic;
 signal count: std_logic_vector(19 downto 0); 
+
 
 -- new signals
 signal data_uico: std_logic_vector (7 downto 0);  --uart in controller out
@@ -125,6 +144,7 @@ signal data_uoci: std_logic_vector (7 downto 0); -- uart out controller in
 signal write_sig, read_sig, data_ready_sig: std_logic; 
 signal mine_signal: std_logic;
 signal mine_out_signal: std_logic;
+
 
 begin
 LB1: inputbuffer port map(	clk		=>	clk,
@@ -136,6 +156,7 @@ LB1: inputbuffer port map(	clk		=>	clk,
 				sensor_l_out	=>	sensor_l,
 				sensor_m_out	=>	sensor_m,
 				sensor_r_out	=>	sensor_r);
+
 
 M1: mine_sensor_top port map (	clk		=>	clk,
 				mine_sensor_in	=> 	sensor_mine_in,
@@ -154,9 +175,21 @@ U1:	uart port map(
 					buffer_empty => 	buffer_empty,
 					write => 		write_sig,     
 
+
+					--data_out(7)	=>	led_7,
+					--data_out(6)	=>	led_6,
+					--data_out(5)	=>	led_5,
+					--data_out(4)	=>	led_4,
+					--data_out(3)	=>	led_3,
+					--data_out(2)	=>	led_2,
+					--data_out(1)	=>	led_1,
+					--data_out(0)	=>	led_0,
+					
 					data_out => 		data_uoci,
 					data_ready => 		data_ready_sig,  
-					read => 		read_sig );
+					read => 		read_sig
+					
+);
 
 LB2: controller port map(	
 				clk			=>	clk,
@@ -169,13 +202,23 @@ LB2: controller port map(
 				count_in		=>  	count,
 
 --new inputs
+
 				data_in			=> 	data_uoci,
 				data_ready		=> 	data_ready_sig, 
 				mine_s			=> 	mine_out_signal,
 
 
 --new outputs
-				data_out		=> 	data_uico,
+
+				data_out(7)	=>	led_7,
+				data_out(6)	=>	led_6,
+				data_out(5)	=>	led_5,
+				data_out(4)	=>	led_4,
+				data_out(3)	=>	led_3,
+				data_out(2)	=>	led_2,
+				data_out(1)	=>	led_1,
+				data_out(0)	=>	led_0,
+				--data_out		=> 	data_uico,
 				write_data		=> 	write_sig,
 				read_data		=> 	read_sig,
 --
@@ -186,17 +229,22 @@ LB2: controller port map(
 				motor_l_direction	=>	motor_l_d,
 
 				motor_r_reset		=>	motor_r_rs,
-				motor_r_direction	=>	motor_r_d);
+				motor_r_direction	=>	motor_r_d
+				);
 
 
 T1: timebase port map(		clk		=>	clk,
 				reset		=>	count_rs,
-				count_out	=>	count);
+				count_out	=>	count,
+				reset_out	=>	reset_periodical       			-- added reset periodical (new ports)
+);
 
 MCL: motorcontrol port map(	clk		=>	clk,
 				reset		=>	motor_l_rs,
 				direction	=>	motor_l_d,
 				count_in	=>	count,
+				
+				reset_in	=>	reset_periodical,			-- added reset periodical (new ports)
 
 				pwm		=>	motor_l_pwm);
 
@@ -207,6 +255,8 @@ MCR: motorcontrol port map(	clk		=>	clk,
 				direction	=>	motor_r_d,
 				count_in	=>	count,
 
+				reset_in	=>	reset_periodical,			-- added reset periodical (new ports)
+				
 				pwm		=>	motor_r_pwm);
 
 
