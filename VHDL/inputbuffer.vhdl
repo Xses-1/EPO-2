7bf97690e8c875e9eb476a36323da5394aaefc8b
Wilson <98850345+wilsonrng@users.noreply.github.com>
2023-06-15 14:44:02 +0200

    inputbuffer EIND CODE 15 juni

--- VHDL/inputbuffer.vhdl
+++ VHDL/inputbuffer.vhdl
@@ -1,62 +1,56 @@
-library IEEE ;
+library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+-- Please add necessary libraries:
+
 
 entity inputbuffer is
-        port (  clk      : in std_logic;
-		sensor_l_in : in std_logic;
-		sensor_r_in : in std_logic;
-		sensor_m_in : in std_logic;
-		sensor_l_out   : out std_logic;
-		sensor_r_out   : out std_logic;
-		sensor_m_out   : out std_logic
+	port (	clk		: in	std_logic;
 
-        );
+		sensor_l_in	: in	std_logic;
+		sensor_m_in	: in	std_logic;
+		sensor_r_in	: in	std_logic;
+
+		sensor_l_out	: out	std_logic;
+		sensor_m_out	: out	std_logic;
+		sensor_r_out	: out	std_logic
+	);
 end entity inputbuffer;
 
-library IEEE ;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
 
-entity shift is
-	port (  clk	: in std_logic;
-		sr_in	: in std_logic;
-		sr_out	: out std_logic
-	);
-end entity shift;
 
-architecture behavioural of shift is
-
-	signal sr : unsigned (31 downto 0); -- This is a vector that defines the length
-					    -- of your shift register
-
-	begin
-		process ( clk )
-		begin
-			if ( rising_edge ( clk )) then
-
-				sr <= shift_left ( sr, 1 );
-				sr ( sr'low ) <= sr_in;		-- You have to comment that out
-				--sr_out <= and sr;		-- and uncomment this line
-								-- to do the proper debouncing
-				sr_out <= sr ( sr'high );
-				
-			end if;
-		end process;
-end architecture behavioural;
 
 architecture structural of inputbuffer is
-	
-	component shift
-		port ( clk 	: in std_logic;
-			sr_in	: in std_logic;
-			sr_out	: out std_logic
-		);
-	end component;
 
-	begin
-		lbl1: shift port map ( clk => clk, sr_in => sensor_l_in, sr_out => sensor_l_out );
-		lbl2: shift port map ( clk => clk, sr_in => sensor_m_in, sr_out => sensor_m_out );
-		lbl3: shift port map ( clk => clk, sr_in => sensor_r_in, sr_out => sensor_r_out );
+component flipflop is 
+
+	port(clk, sensor_l_in, sensor_m_in, sensor_r_in: in std_logic;  sensor_l_out, sensor_m_out, sensor_r_out: out std_logic);
+end component;
+
+signal s1, s2, s3: std_logic;
+
+begin 
+
+FF1: flipflop port map(	clk => clk, 
+
+			sensor_l_in => sensor_l_in, 
+			sensor_m_in => sensor_m_in,  
+			sensor_r_in => sensor_r_in, 
+
+			sensor_l_out => s1,
+			sensor_m_out => s2,
+			sensor_r_out => s3);
+
+FF2: flipflop port map(	clk => clk, 
+
+			sensor_l_in => s1,
+			sensor_m_in => s2,
+			sensor_r_in => s3,
+
+			sensor_l_out => sensor_l_out,
+			sensor_m_out => sensor_m_out,
+			sensor_r_out => sensor_r_out);
 
 end structural;
+
+			
